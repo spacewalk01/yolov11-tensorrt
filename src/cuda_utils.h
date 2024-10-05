3d820259bb67cd66a6e80459a5d7bc2aad0dde61
spacewalk01 <selmee006@gmail.com>
2024-10-05 23:44:18 +0900

    Update cuda_utils.h

--- src/cuda_utils.h
+++ src/cuda_utils.h
@@ -14,16 +14,4 @@
     }
 #endif  // CUDA_CHECK
 
-#define CHECK_RETURN_W_MSG(status, val, errMsg)                                                                        \
-    do                                                                                                                 \
-    {                                                                                                                  \
-        if (!(status))                                                                                                 \
-        {                                                                                                              \
-            sample::gLogError << errMsg << " Error in " << __FILE__ << ", function " << FN_NAME << "(), line " << __LINE__     \
-                      << std::endl;                                                                                    \
-            return val;                                                                                                \
-        }                                                                                                              \
-    } while (0)
-
-
 #endif  // TRTX_CUDA_UTILS_H_
